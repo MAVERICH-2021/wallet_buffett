60118fff71b77133062d7b446f0b3f7ec6f89715
mave <1141423570wn@gmail.com>
2024-01-24 13:53:17 +0800

    init backend

---
+++ backend/src/main/java/com/example/spring/wallet_buffett/WalletBuffettApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.wallet_buffett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletBuffettApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletBuffettApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/spring/wallet_buffett/WalletBuffettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.wallet_buffett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletBuffettApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
